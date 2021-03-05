148dd6d61c0f11e2caf856823c7d75f5abf62397
Mateusz Rudkiewicz <mateusz_19@o2.pl>
2021-03-05 21:34:04 +0100

    Rev.03

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,44 +1,48 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import com.sun.istack.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @GetMapping("getTasks") //  = @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
-    @GetMapping
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    @GetMapping("getTask/{taskId}") // = @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    public TaskDto getTask(@PathVariable @NotNull Long taskId) {
+        long id = service.getTask(taskId).get().getId();
+        String title = service.getTask(taskId).get().getTitle();
+        String content = service.getTask(taskId).get().getContent();
+        return new TaskDto(id, title, content);
     }
 
-    @DeleteMapping
+    @DeleteMapping("deleteTask") // = @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId) {
-
     }
 
-    @PutMapping
+    @PutMapping("updateTask") // = @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
 
-    @PostMapping
+    @PostMapping("createTask") // = @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto) {
-
     }
+
 }
