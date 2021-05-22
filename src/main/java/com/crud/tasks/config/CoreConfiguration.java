5de449396f6543ca868fb7afb5371158c8fbe7a8
Mateusz Rudkiewicz <mateusz_19@o2.pl>
2021-05-22 23:54:07 +0200

    23.4 Implementacja maila w projekcie oraz adnotaca @Scheduled

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -8,7 +8,9 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @Configuration
 @EnableSwagger2
 public class CoreConfiguration {
